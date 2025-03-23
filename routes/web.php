a46572fcfb6c24a7907b13060226632da55132d5
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2025-03-23 20:58:47 -0300

    rota login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('auth.login');
 });
 
 Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
