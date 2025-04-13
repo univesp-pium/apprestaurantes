f4e421b565a76a3e8525096b79a159e8ccef5619
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-04-13 20:20:19 -0300

    Exemplo tabelas

---
+++ app/Models/City.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    protected $table = 'cities';
+
+    protected $fillable = [
+        'state_id',
+        'name',
+    ];
+
+    // Relationships
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+}
