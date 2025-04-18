49ea5d88ba41dd07988798159559e17b260400be
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-04-18 11:23:40 -0300

    Remoção de código não utilizado

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -10,9 +10,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
         id="main-font-link">
-    <link rel="stylesheet" href="{{ asset('admin/assets/fonts/tabler-icons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/fonts/feather.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/fonts/material.css') }}">
+
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}" id="main-style-link">
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style-preset.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
@@ -35,20 +33,12 @@
 
     @include('admin.layouts.footer')
 
-    <script src="{{ asset('admin/assets/js/apexcharts.min.js') }}"></script>
-    {{-- <script src="{{ asset('admin/assets/js/dashboard-default.js') }}"></script> --}}
     <script src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('admin/assets/js/simplebar.min.js') }}"></script>
     <script src="{{ asset('admin/assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/custom-font.js') }}"></script>
     <script src="{{ asset('admin/assets/js/pcoded.js') }}"></script>
     <script src="{{ asset('admin/assets/js/feather.min.js') }}"></script>
     <script>
-        // layout_change('light');
-        change_box_container('false');
-        layout_rtl_change('false');
-        preset_change("preset-1");
-        font_change("Public-Sans");
     </script>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     @stack('scripts')
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -4,12 +4,12 @@
             <ul class="list-unstyled">
                 <li class="pc-h-item pc-sidebar-collapse">
                     <a href="#" class="pc-head-link ms-0" id="sidebar-hide">
-                        <i class="ti ti-menu-2"></i>
+                        <i class="fa-solid fa-bars"></i>
                     </a>
                 </li>
                 <li class="pc-h-item pc-sidebar-popup">
                     <a href="#" class="pc-head-link ms-0" id="mobile-collapse">
-                        <i class="ti ti-menu-2"></i>
+                        <i class="fa-solid fa-bars"></i>
                     </a>
                 </li>
                 <li class="dropdown pc-h-item d-inline-flex d-md-none">
@@ -27,18 +27,11 @@
                         </form>
                     </div>
                 </li>
-                <li class="pc-h-item d-none d-md-inline-flex">
-                    {{-- <form class="header-search">
-                        <i data-feather="search" class="icon-search"></i>
-                        <input type="search" class="form-control" placeholder="Search here. . .">
-                    </form> --}}
-                </li>
             </ul>
         </div>
         <!-- [Mobile Media Block end] -->
         <div class="ms-auto">
             <ul class="list-unstyled">
-
                 <li class="dropdown pc-h-item header-user-profile">
                     <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
                         role="button" aria-haspopup="false" data-bs-auto-close="outside" aria-expanded="false">
@@ -51,31 +44,8 @@
                                 aria-labelledby="drp-t1" tabindex="0">
 
                                 <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-power"></i>
+                                    <i class="fa-solid fa-power-off"></i>
                                     <span>Logout</span>
-                                </a>
-                            </div>
-                            <div class="tab-pane fade" id="drp-tab-2" role="tabpanel" aria-labelledby="drp-t2"
-                                tabindex="0">
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-help"></i>
-                                    <span>Support</span>
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-user"></i>
-                                    <span>Account Settings</span>
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-lock"></i>
-                                    <span>Privacy Center</span>
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-messages"></i>
-                                    <span>Feedback</span>
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ti ti-list"></i>
-                                    <span>History</span>
                                 </a>
                             </div>
                         </div>
