ff434c90739ae7416913a80d9248e6e0827b3fe4
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-04-18 12:11:00 -0300

    Remoção de código não utilizado

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Painel Administrativo')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/default.css') }}">
     @vite('resources/css/app.css')
     @stack('styles')
 </head>
