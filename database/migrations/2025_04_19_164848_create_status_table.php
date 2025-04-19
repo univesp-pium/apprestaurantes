8fe42779c9428fa68d6346bfe783961d092a0fe4
Fernando Rodrigues <ferando_tecti@hotmail.com>
2025-04-19 17:58:51 -0300

    Correção de migration

--- database/migrations/2025_04_19_164848_create_status_table.php
+++ database/migrations/2025_04_19_164848_create_status_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('status', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('active');->default(true);
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
